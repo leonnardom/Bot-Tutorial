a7d985c556f5da199d77569def55179cfccdbc0e
zSpl1nterUS_ <metalherogamer@hotmail.com>
2020-12-24 06:51:58 -0300

    ok

--- src/database/Schemas/Guild.js
+++ src/database/Schemas/Guild.js
@@ -4,10 +4,6 @@ const Schema = mongoose.Schema;
 let guildSchema = new Schema({
   _id: { type: String, required: true },
   prefix: { type: String, default: "z!" },
-  addBot: {
-    lastUser: {type: String, default: "null"},
-    time: {type: Number, default: 0}
-  }
 });
 
 let Guild = mongoose.model("Guilds", guildSchema)
