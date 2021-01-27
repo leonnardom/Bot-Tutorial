4c8a29f61150205954c4845db58320b1c76a63d6
zSpl1nterUS_ <metalherogamer@hotmail.com>
2021-01-26 21:53:34 -0300

    update

--- src/commands/Economy/work.js
+++ src/commands/Economy/work.js
@@ -3,6 +3,14 @@ const Command = require("../../structures/Command");
 const ClientEmbed = require("../../structures/ClientEmbed");
 const moment = require("moment");
 require("moment-duration-format");
+const { MessageAttachment } = require("discord.js");
+const { loadImage, registerFont, createCanvas } = require("canvas");
+registerFont("src/assets/fonts/Montserrat-Black.ttf", { family: "Montserrat" });
+registerFont("src/assets/fonts/Segoe Print.ttf", { family: "Segoe Print" });
+registerFont("src/assets/fonts/Segoe UI.ttf", { family: "Segoe UI" });
+registerFont("src/assets/fonts/Segoe UI Black.ttf", {
+  family: "Segoe UI Black",
+});
 
 module.exports = class Work extends (
   Command
@@ -51,20 +59,19 @@ module.exports = class Work extends (
                 user.work.level + 1
               }**.`
             );
+            await User.findOneAndUpdate(
+              { idU: message.author.id },
+              {
+                $set: {
+                  "work.cooldown": Date.now(),
+                  "work.exp":
+                    user.work.exp + xp > nextlevel ? 0 : user.work.exp + xp,
+                  coins: user.coins + money,
+                  "work.level": user.work.level + 1,
+                },
+              }
+            );
           }
-
-          await User.findOneAndUpdate(
-            { idU: message.author.id },
-            {
-              $set: {
-                "work.cooldown": Date.now(),
-                "work.exp":
-                  user.work.exp + xp > nextlevel ? 0 : user.work.exp + xp,
-                coins: user.coins + money,
-                "work.level": user.work.level + 1,
-              },
-            }
-          );
         }
       });
       return;
@@ -106,47 +113,146 @@ module.exports = class Work extends (
         message.author;
 
       User.findOne({ idU: USER.id }, async (err, user) => {
-        const work = user.work;
-        const money = Math.ceil(work.level * 2 * work.coins + 200);
+        const canvas = createCanvas(400, 600);
+        const ctx = canvas.getContext("2d");
+        let xp = Math.floor(Math.random() * 50) + 1;
+        let work = user.work.cooldown;
         let cooldown = 2.88e7;
-        let cool = work.cooldown;
+        let money = Math.ceil(user.work.level * 2 * user.work.coins + 200);
+        let nextlevel = user.work.nextLevel * user.work.level;
+        let works = work - (Date.now() - cooldown);
 
-        const INFO = new ClientEmbed(author)
-          .setAuthor(
-            `${USER.username} Empresa`,
-            USER.displayAvatarURL({ dynamic: true })
-          )
-          .setThumbnail(
-            USER.displayAvatarURL({ dynamic: true, size: 2048, format: "jpeg" })
-          )
-          .addFields(
-            {
-              name: "Nome da Empresa",
-              value: work.name == "null" ? "Nome não definido" : work.name,
-            },
-            {
-              name: "Level/XP",
-              value: `#${work.level} - ${work.exp}/${
-                work.level * work.nextLevel
-              }`,
-            },
-            {
-              name: "Salário",
-              value: `${money.toLocaleString()} coins`,
-            },
-            {
-              name: `Cooldown`,
-              value:
-                cooldown - (Date.now() - cool) < 0
-                  ? "Pode trabalhar"
-                  : moment
-                      .duration(cooldown - (Date.now() - cool))
-                      .format("h [horas], m [minutos] e s [segundos]")
-                      .replace("minsutos", "minutos"),
-            }
-          );
+        const background = await loadImage(
+          "./src/assets/img/jpeg/Work_Template.png"
+        );
+        ctx.drawImage(background, 0, 0, 400, 600);
 
-        message.quote(INFO);
+        //========================// Import Texts //========================//
+
+        ctx.textAlign = "center";
+        ctx.font = '40px "Montserrat"';
+        ctx.fillStyle = "#ffffff";
+        ctx.fillText(
+          message.author.username.length > 20
+            ? message.author.username.slice(0, 20) + "..."
+            : message.author.username,
+          200,
+          210
+        );
+
+        ctx.textAlign = "left";
+        ctx.font = '30px "Segoe UI Black"';
+        ctx.fillStyle = "#000000";
+        ctx.fillText("Nome\nStatus\nLevel", 5, 260);
+        ctx.fillText("Salário", 5, 500);
+
+        ctx.textAlign = "right";
+        ctx.font = '25px "Segoe UI Black"';
+        ctx.fillStyle = "#000000";
+        ctx.fillText(
+          user.work.name.length > 15
+            ? user.work.name.slice(0, 15) + "..."
+            : user.work.name,
+          390,
+          260
+        );
+        ctx.fillText(
+          works < 0
+            ? "Pode Trabalhar"
+            : moment
+                .duration(cooldown - (Date.now() - work))
+                .format("h[h], m[m], s[s]"),
+          390,
+          300
+        );
+        ctx.fillText(user.work.level, 390, 340);
+        ctx.fillText(`${money.toLocaleString()} coins`, 390, 500);
+
+        //========================// Import ProgressBar //========================//
+
+        const need = user.work.level * user.work.nextLevel;
+
+        let widthXp = (user.work.exp * 350) / need;
+        if (user.work.exp > 350 - 18.5) user.work.xp = 200 - 18.5;
+
+        ctx.beginPath();
+        ctx.fillStyle = "#404040";
+        ctx.arc(
+          5 + 18.5,
+          340 + 18.5 + 36.25,
+          18.5,
+          1.5 * Math.PI,
+          0.5 * Math.PI,
+          true
+        );
+        ctx.fill();
+        ctx.fillRect(5 + 18.5, 340 + 36.25, 350 - 18.5, 37.5);
+        ctx.arc(
+          5 + 350,
+          340 + 18.5 + 36.25,
+          18.75,
+          1.5 * Math.PI,
+          0.5 * Math.PI,
+          false
+        );
+        ctx.fill();
+
+        ctx.beginPath();
+        ctx.fillStyle = "#ff4d4d";
+        ctx.arc(
+          5 + 18.5,
+          340 + 18.5 + 36.25,
+          18.5,
+          1.5 * Math.PI,
+          0.5 * Math.PI,
+          true
+        );
+        ctx.fill();
+        ctx.fillRect(5 + 18.5, 340 + 36.25, widthXp, 37.5);
+        ctx.arc(
+          5 + 18.5 + widthXp,
+          340 + 18.5 + 36.25,
+          18.75,
+          1.5 * Math.PI,
+          0.5 * Math.PI,
+          false
+        );
+        ctx.fill();
+
+        ctx.textAlign = "center";
+        ctx.font = "30px 'Calibri'";
+        ctx.fillStyle = "#ffffff";
+        ctx.fillText(
+          `${user.work.exp.toLocaleString()}/${(
+            user.work.level * user.work.nextLevel
+          ).toLocaleString()}`,
+          200,
+          405
+        );
+
+        //========================// Import Avatar //========================//
+
+        ctx.beginPath();
+        ctx.arc(200, 70, 65, 0, Math.PI * 2, true);
+        ctx.lineWidth = 3;
+        ctx.strokeStyle = "#faf5f5";
+        ctx.stroke();
+        ctx.closePath();
+        ctx.clip();
+
+        const avatar = await loadImage(
+          USER.displayAvatarURL({ format: "jpeg" })
+        );
+        ctx.drawImage(avatar, 135, 5, 130, 130);
+
+        //===============================// //================================//
+
+        const attach = new MessageAttachment(
+          canvas.toBuffer(),
+          `Work_${USER.tag}_.png`
+        );
+
+        message.quote(attach);
       });
     }
   }
